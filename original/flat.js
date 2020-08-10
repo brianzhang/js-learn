90b5708d369d5ae1929b803174578ce81bf080b0
brian_zhangfan <7464931+brian_zhangfan@user.noreply.gitee.com>
2020-08-10 14:37:51 +0800

    数组展开

--- original/flat.js
+++ original/flat.js
@@ -1,7 +1,7 @@
 // 递归法
-function flat (arr) {
+function flat(arr) {
   const result = [] // 利用闭包缓存我们的结果
-  function _flat (arr) {
+  function _flat(arr) {
     arr.forEach(element => {
       if (Array.isArray(element)) { // 递归条件
         _flat(element)
@@ -14,10 +14,10 @@ function flat (arr) {
   return result
 }
 // 广度优先,不能保证顺序
-function flat2 (arr) {
+function flat2(arr) {
   const result = [] // 储存结果
   const list = [] // 队列
-  function _forEach (arr) {
+  function _forEach(arr) {
     arr.forEach(el => {
       if (Array.isArray(el)) list.push(el) // item 如果是数组，将子项依次推入队列
       else result.push(el) // item 如果是字符串，将子项推入结果
@@ -32,8 +32,10 @@ function flat2 (arr) {
 }
 
 // 取巧,你得确定你的数据里面没有字符串 ','
-function flat3 (arr) {
+function flat3(arr) {
   return arr.toString().split(',').map(value => +value)
 }
 
 console.log('object', flat([1, [2, 3, [4, 5], 6], 7]))
+console.log('object', flat2([1, [2, 3, [4, 5], 6], 7]))
+console.log('object', flat3([1, [2, 3, [4, 5], 6], 7]))
